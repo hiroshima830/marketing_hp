2a067f0e1c9eb8b81c32f2177005dc0ab1819a8d
noriyamada <58667742+hiroshima830@users.noreply.github.com>
2023-06-10 13:16:11 +0900

    顔写真だけ

--- main.py
+++ main.py
@@ -1,12 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib, settings
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    sender_email = 'bullpen.hiroshima@gmail.com'  # 送信元のメールアドレス
+    sender_password = settings.mailpass  # 送信元のメールアカウントのパスワード
+
+    # メールの作成
+    email = request.form['email']
+    name = request.form['name']
+
+    subject = f'初回無料相談に関するお問い合わせ'
+    body = f'{name}様\n' \
+           f'ご連絡ありがとうございます。お手数ですが、以下の質問にお答えいただき返信をお願い致します。\n' \
+           '\n' \
+           f'1.貴社名\n' \
+           '\n' \
+           f'2.貴社ウェブサイトURL\n' \
+           '\n' \
+           f'3.ご自身のビジネスについて教えてください。具体的な商品やサービス、ターゲットなどの情報をお教えいただけると助かります。\n' \
+           '\n' \
+           f'4.集客のために既に試してみたことや、経験した成果について教えていただけますか？\n' \
+           '\n' \
+           f'5.その他お問合せ、ご相談したい内容はございますか？\n' \
+           '\n' \
+           f'5.上記の質問にお答えいただけましたら、より具体的なご提案や解決策をお伝えすることができます。\n' \
+           f'※このメールに心当たりのない場合は、お手数ですが削除をお願い致します。\n' \
+           '\n' \
+           f'BULLPEN\n' \
+
+    # メールの作成
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    # メールの送信
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(msg)
+        server.quit()
+        # メールの送信に成功した場合には成功メッセージを返す
+        return render_template('thanks.html')
+    except smtplib.SMTPException:
+        return "メールの送信に失敗しました"
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
